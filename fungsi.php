1c5a45e7608e8ac2d48359454d4b9dcadb583393
immawan <im.maruddani@gmail.com>
2015-01-18 21:40:24 +0700

    Update
    udah bisa sms problem

--- fungsi.php
+++ fungsi.php
@@ -12,7 +12,7 @@ function koneksi() {
 function insertInbox($pengirim, $pesan) {
     mysql_query("INSERT INTO inbox VALUES('',curdate(),curtime(),'$pengirim','$pesan')");
 }
-
+/*
 function selectUser($nopengirim) {
 	$user = mysql_query("SELECT iduser FROM user_sms WHERE nohp = '$nopengirim'");
 	if (!$user) {
@@ -26,6 +26,80 @@ function selectNamaUser($iduser) {
 	$nama = mysql_query("SELECT nama FROM user_sms WHERE iduser = '$iduser'");
 	$hasilnama = mysql_fetch_array($nama);
 	return $hasilnama[0];
+}
+*/
+
+function selectDosen($nopengirim) {
+	$user = mysql_query("SELECT iddosen FROM dosen WHERE nohp = '$nopengirim' OR nohpcadangan = '$nopengirim'");
+	if (!$user) {
+		return NULL;
+	} else {
+		$hasiluser = mysql_fetch_array($user);
+		return $hasiluser[0];
+	}
+}
+
+function selectKorwil($nopengirim) {
+	$user = mysql_query("SELECT nokorwil FROM korwil WHERE nohp = '$nopengirim' OR nohpcadangan = '$nopengirim'");
+	if (!$user) {
+		return NULL;
+	} else {
+		$hasiluser = mysql_fetch_array($user);
+		return $hasiluser[0];
+	}
+}
+
+function selectKortim($nopengirim) {
+	$user = mysql_query("SELECT nokortim FROM kortim WHERE nohp = '$nopengirim' OR nohpcadangan = '$nopengirim'");
+	if (!$user) {
+		return NULL;
+	} else {
+		$hasiluser = mysql_fetch_array($user);
+		return $hasiluser[0];
+	}
+}
+
+function cekListing($kota, $kec, $nbs) {
+    $query = mysql_query("SELECT listing FROM nbs 
+                WHERE kodekota='$kota' 
+                AND kodekecamatan='$kec'  
+                AND nbs='$nbs'");
+    $queryArray = mysql_fetch_array($query);
+    return $queryArray[0];
+}
+
+function gantiNomorDosen($nopengirim, $pesan) {
+	$pesanArray = explode(" ", $pesan);
+	$nomorLama = $pesanArray[1];
+	$nomorBaru = $pesanArray[2];
+
+	if ($nomorBaru == "" or strlen($nomorBaru) < 10 OR strlen($nomorBaru) > 14) {
+        $pesanError = "Maaf,+format+nomor+hp+baru+yang+akan+diganti+tidak+sesuai.";
+        return $pesanError;
+    }
+
+    $cek = mysql_query("SELECT iddosen FROM dosen WHERE nohp = '$nomorLama' OR nohpcadangan = '$nomorLama'");
+    $cekArray = mysql_fetch_array($cek);
+    if ($cekArray[0] == "") {
+        $pesanKirim = "Maaf,+nomor+HP+Anda+belum+terdaftar,+silakan+hubungi+CP.";
+        return $pesanKirim;
+    }
+
+    $iddosen = $cekArray[0];
+    $query1 = mysql_query("SELECT nohp FROM dosen WHERE nohp ='$nopengirim' AND iddosen='$iddosen' ");
+    $query2 = mysql_query("SELECT nohpcadangan FROM dosen WHERE nohpcadangan ='$nopengirim' AND iddosen='$iddosen'");
+    $query1FetchArray = mysql_fetch_array($query1);
+    $query2FetchArray = mysql_fetch_array($query2);
+    if ($query1FetchArray[0] != "") {
+        mysql_query("UPDATE dosen SET nohp = '$nomorBaru' WHERE iddosen='$iddosen'");
+    }
+    if ($query2FetchArray[0] != "") {
+        mysql_query("UPDATE dosen SET nohpcadangan = '$nomorBaru' WHERE iddosen='$iddosen'");
+    }
+
+    mysql_query("INSERT INTO rekap_gantinomor VALUES(curdate(),curtime(),'$iddosen','$nomorLama','$nomorBaru')");
+    $pesanKirim = "No+HP+Anda+sudah+di-update+menjadi+$nomorBaru.+Terima+Kasih+:)";
+    return $pesanKirim;
 }
 
 ?>
--- terima.php
+++ terima.php
@@ -8,7 +8,8 @@ $nopengirim = $_GET['pengirim'];
 //membuat nomor pengirim menjadi 08...
 if (substr($nopengirim, 0, 3) == "+62") {
     $nopengirim = "0" . substr($nopengirim, 3, strlen($nopengirim) - 3);
-} else if (substr($nopengirim, 0, 2) == "62") {
+} 
+if (substr($nopengirim, 0, 2) == "62") {
     $nopengirim = "0" . substr($nopengirim, 2, strlen($nopengirim) - 2);
 }
 
@@ -19,26 +20,35 @@ $pesan = strtoupper(trim($_GET['pesan']));
 
 koneksi();
 insertInbox($nopengirim, $pesan);
+$idkortim = selectKortim($nopengirim);
 
-$userid = selectUser($nopengirim);
+//mengetahui apakah yang mengirim ini dosen, korwil, atau kortim
+if($idkortim!=NULL) {
+	$idpengirim = $idkortim;
+} 
 
-if ($userid != NULL) {
-	koneksi();
-	$namauser = selectNamaUser($userid);
-	if(substr($pesan, 0, 3)=="TES") {
-		if(substr($pesan, 4, 3)=="SMS") {
-			$pesanKirim = "Tes+Sms+Berhasil,+".changeSpaceToPlus($namauser);
-			trigger($nopengirim, $pesanKirim);
-		} else {
-			$pesanKirim = "Sms+Berhasil+dikirim,+".changeSpaceToPlus($namauser);
-			trigger($nopengirim, $pesanKirim);
-		}
-	} else {
-		$pesanKirim = "Format+SMS+Anda+salah,+".changeSpaceToPlus($namauser);
+if($idpengirim==NULL) {
+	$pesanKirim = "Maaf,+nomor+Anda+tidak+terdaftar+dalam+database+kami.+Silahkan+hubungi+admin.";
+	trigger($nopengirim, $pesan);
+}
+
+if (substr($pesan, 0, 1) == "G") { //untuk ganti nomor (baru bisa buat dosen)
+	$pesanKirim = gantiNomorDosen($nopengirim, $pesan);
+	trigger($nopengirim, $pesanKirim);
+} elseif (substr($pesan, 0, 1) == "P") { //untuk problem
+	$isiProblem = substr($_GET['pesan'], 2);
+	mysql_query("INSERT INTO problem VALUES('',curdate(),curtime(),'$idpengirim','$isiProblem',tanggapan)");
+	$pesanKirim = "Kendala+berhasil+dilaporkan.+Terima+kasih.";
+	trigger($nopengirim, $pesanKirim);
+} elseif (substr($pesan, 0, 2) == "EL") { //untuk entry listing
+	if (substr($idpengirim, 0, 2) !== "KT") { //hanya bisa dilakukan oleh Kortim
+		$pesanKirim = "Maaf,+Anda+tidak+memiliki+akses+untuk+entry+listing.";
 		trigger($nopengirim, $pesanKirim);
-	}	
+	} else {
+		#code...
+	}
 } else {
-	$pesanKirim = "Maaf,+nomor+Anda+tidak+terdaftar+di+Database+kami.";
+	$pesanKirim = "Maaf,+format+sms+Anda+tidak+dikenali.";
 	trigger($nopengirim, $pesanKirim);
 }
 
